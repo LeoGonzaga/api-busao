44619ed0f4f92957649db01ea9a3c4ca2f998ceb
LeoGonzaga <sacredlotus05@gmail.com>
2020-10-23 22:48:17 -0300

    removendo nm

--- routes.js
+++ routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const Bus = require("./src/Controllers/busController/BusController");
 const routes = express.Router();
 
-// Produtos
+// Rotas
 routes.get("/all", Bus.getAllBus);
 routes.post("/create", Bus.createRoute);
 routes.post("/city", Bus.getBusByCity)
+
 module.exports = routes;
