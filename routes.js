b712e3c69049bcc4f60a58aad1f8a0f18b8f8346
Renan Bizarri <renanbizarri@hotmail.com>
2021-01-10 17:14:59 -0300

    Busca Geral e Busca do Maior e Menor Horario

--- routes.js
+++ routes.js
@@ -9,9 +9,12 @@ const Company = require("./src/Controllers/companyController/CompanyController")
 // Rotas
 routes.get("/Buses", Bus.getAllBus);
 routes.get("/Names", Bus.cityNames);
+routes.get("/busFind", Bus.getBus);
+routes.get("/hour", Bus.getHour);
 routes.post("/createRoute", Bus.createRoute);
 routes.post("/busByCity", Bus.getBusByCity);
 routes.post("/busByHour", Bus.getBusByHour);
+
 routes.post("/createUser", User.createUser);
 routes.post("/login", User.login);
 
