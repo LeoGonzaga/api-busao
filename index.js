85368194d556fcd98a8173f684a0ce683ea0e4b4
LeoGonzaga <sacredlotus05@gmail.com>
2021-01-19 21:12:25 -0300

    adicao do modo de dev ao .env

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ require("dotenv").config();
 const app = express();
 const port = 3003;
 app.use(cors());
-mongoose.connect(
-  "mongodb+srv://superadmin:qwe123leo@cluster0.fcq5c.mongodb.net/web?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  }
-);
+
+if (process.env.DEV == "true") {
+  console.log("MODO DE TESTE");
+  mongoose.connect(
+    "mongodb+srv://games:games@omnistack.ekd7k.mongodb.net/busao?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  );
+} else {
+  mongoose.connect(
+    "mongodb+srv://superadmin:qwe123leo@cluster0.fcq5c.mongodb.net/web?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  );
+}
 
 app.use(express.json());
 app.use(routes);
