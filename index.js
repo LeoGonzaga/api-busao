336a6f82d4d12c17eaca0f1c420d7569c32fb00b
LeoGonzaga <sacredlotus05@gmail.com>
2021-01-23 17:48:31 -0300

    correcao do env

--- index.js
+++ index.js
@@ -7,28 +7,12 @@ const app = express();
 const port = 3003;
 app.use(cors());
 
-if (process.env.DEV == "true") {
-  console.log("MODO DE TESTE");
-  mongoose.connect(
-    "mongodb+srv://games:games@omnistack.ekd7k.mongodb.net/busao?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  );
-} else {
-  mongoose.connect(
-    "mongodb+srv://superadmin:qwe123leo@cluster0.fcq5c.mongodb.net/web?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  );
-}
+mongoose.connect(process.env.mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
 app.use(express.json());
 app.use(routes);
